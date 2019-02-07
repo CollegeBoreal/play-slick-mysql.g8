c4c4c5f387dbb335c86d38801b3331b4ca066bd9
setrar <setrar@valiha.com>
2019-02-07 11:17:11 -0500

    Update SlickInMemorySpec.scala

--- src/main/g8/tests/common/SlickInMemorySpec.scala
+++ src/main/g8/tests/common/SlickInMemorySpec.scala
@@ -8,25 +8,21 @@ import play.api.db.DBApi
 import play.api.db.evolutions.Evolutions
 import play.api.inject.guice.GuiceApplicationBuilder
 
-abstract class SlickInMemorySpec
-    extends PlaySpec
-    with GuiceOneAppPerSuite
-    with BeforeAndAfter {
-  implicit override lazy val app: Application = {
-    val dbName = s"play-test-${scala.util.Random.nextInt()}"
-    val dbUrl = s"jdbc:h2:mem:$dbName;MODE=PostgreSQL;DATABASE_TO_UPPER=false"
+abstract class SlickInMemorySpec extends PlaySpec with GuiceOneAppPerSuite with BeforeAndAfter {
+  implicit override lazy val app : Application = {
+    val dbName = s"play-test-$"$"${scala.util.Random.nextInt()}"
+    val dbUrl = s"jdbc:h2:mem:$"$"$dbName;MODE=PostgreSQL;DATABASE_TO_UPPER=false"
 
     new GuiceApplicationBuilder()
-      .configure(
-        Map(
-          "slick.dbs.default" -> Map(
-            "profile" -> "slick.jdbc.H2Profile$",
-            "db" -> Map(
-              "driver" -> "org.h2.Driver",
-              "url" -> dbUrl
-            )
+      .configure(Map(
+        "slick.dbs.default" -> Map(
+          "profile" -> "slick.jdbc.H2Profile$"$"$",
+          "db" -> Map(
+            "driver" -> "org.h2.Driver",
+            "url" -> dbUrl
           )
-        ))
+        )
+      ))
       .in(Mode.Test)
       .build
   }
