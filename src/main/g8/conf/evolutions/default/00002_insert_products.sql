31cf0b62b281323b684c5688e4bb11d10b64d647
setrar <setrar@valiha.com>
2019-04-08 18:40:44 -0400

    Update 00002_insert_products.sql

--- src/main/g8/conf/evolutions/default/00002_insert_products.sql
+++ src/main/g8/conf/evolutions/default/00002_insert_products.sql
@@ -2,11 +2,11 @@
 
 # --- !Ups
 
-INSERT INTO products (sku, name, description)
-VALUES ('bppen-red-na', 'Excella Pen', 'Ballpoint click pen with red body and black clicker, white Excella logo'),
-       ('stynot-wht-std', 'Excella Sticky Notes', 'Standard-sized sticky notes with red and black Excella branding'),
-       ('mug-gry-sma', 'Excella Mug', '8oz gray mug with white Excella branding and red-orange interior'),
-       ('tmblr-stl-med', 'Excella Steel Tumbler', '16oz Steel Tumbler with black Excella branding');
+INSERT INTO products (sku, name, description, updated)
+VALUES ('bppen-red-na', 'Excella Pen', 'Ballpoint click pen with red body and black clicker, white Excella logo', NOW()),
+       ('stynot-wht-std', 'Excella Sticky Notes', 'Standard-sized sticky notes with red and black Excella branding', NOW()),
+       ('mug-gry-sma', 'Excella Mug', '8oz gray mug with white Excella branding and red-orange interior', NOW()),
+       ('tmblr-stl-med', 'Excella Steel Tumbler', '16oz Steel Tumbler with black Excella branding', NOW());
 
 # --- !Downs
 
