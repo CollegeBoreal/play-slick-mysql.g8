01c540fba79a92bf242a383b39b124ae47f43feb
setrar <setrar@valiha.com>
2019-04-08 19:07:50 -0400

    Update Models.scala

--- src/main/g8/app/models/Models.scala
+++ src/main/g8/app/models/Models.scala
@@ -5,6 +5,7 @@ case class Message(message: String)
 case class Product(id: Option[Int],
                    sku: String,
                    name: String,
-                   description: String)
+                   description: String,
+                   updated: java.time.LocalDateTime)
 
 case class Customer(customer: Option[Int], name: String, phone: String)
