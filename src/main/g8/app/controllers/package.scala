6ef85adb0c97bee5e1f0c31a31ecf3306a6faad5
setrar <setrar@valiha.com>
2019-04-19 13:01:23 -0400

    Update package.scala

--- src/main/g8/app/controllers/package.scala
+++ src/main/g8/app/controllers/package.scala
@@ -1,12 +1,13 @@
+import java.sql.Timestamp
+
 package object controllers {
 
   import play.api.libs.json._
 
   implicit object timestampFormat extends Format[java.sql.Timestamp] {
     val format = new java.text.SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
-    def reads(json: JsValue) =
-      JsSuccess(new java.sql.Timestamp(format.parse(json.as[String]).getTime))
-    def writes(ts: java.sql.Timestamp) = JsString(format.format(ts))
+    def reads(json: JsValue): JsSuccess[Timestamp] = JsSuccess(new java.sql.Timestamp(format.parse(json.as[String]).getTime))
+    def writes(ts: java.sql.Timestamp): JsString = JsString(format.format(ts))
   }
 
 }
